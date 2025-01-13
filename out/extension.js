30c81b2c41191b9937a95f14a8dbde3572c176f4
mylifewithviolin <123276433+mylifewithviolin@users.noreply.github.com>
2025-01-13 14:15:31 +0900

    [ADD] version 0.0.1

---
+++ out/extension.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = activate;
+exports.deactivate = deactivate;
+function activate(context) {
+    // ���̕����ɋ@�\��ǉ����܂�
+}
+function deactivate() { }
+//# sourceMappingURL=extension.js.map
---
+++ src/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    // ‚±‚Ì•”•ª‚É‹@”\‚ð’Ç‰Á‚µ‚Ü‚·
+}
+
+export function deactivate() {}
